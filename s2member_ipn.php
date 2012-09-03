771e09e43e378a4f4f033ac599e13d9dee4ecea6
BaMF <codebreaker_boy07@yahoo.com>
2012-09-03 12:14:38 +0300

    1.1
    The ipn creates the wp user and sets the right role.
    First version only handles user creation, no update on OTO.

--- s2member_ipn.php
+++ s2member_ipn.php
@@ -1,18 +1,4 @@
 <?php
-	//You get this URL by going to : s2Member -> Payapl Options -> Paypal IPN Integration -> IPN /w Proxy key
-	//You just copy that in and replace [proxy-gateway] with WSO_Integration
-//	$post_url='http://localhost/wordpress/?s2member_paypal_notify=1&s2member_paypal_proxy=WSO_Integration&s2member_paypal_proxy_verification=b6234ab61f38faa6ec11c29b609300d5';
-
-	$post_url='http://www.authoritybloggingformula.com/?s2member_paypal_notify=1&s2member_paypal_proxy=WSO&s2member_paypal_proxy_verification=e37e2d1cfcb7900f24d6e6c77b25dac7';
-	// Stop editing from here on 
-
-
-	//Prepare the custom var required by s2Member, it should contain the domain name.
-	$custom=explode('/', $post_url);
-	$custom=$custom['2'];
-	$custom=str_replace('www.','', $custom);
-
-
 	if($_POST)
 	{
 		$name=$_POST['item_name1'];
@@ -21,9 +7,9 @@
 		$name=strtolower($name);
 
 		if(strstr($name, 'oto'))
-			$item_number=2;
+			$role='s2member_level2';
 		else
-			$item_number=1;
+			$role='s2member_level1';
 
 		$options=$_POST;
 
@@ -33,23 +19,40 @@
 		fwrite($fp, $content);
 		fclose($fp);
 
-		$options['item_number']=$item_number;
-		$options['custom']=$custom;
-
-		http_post($post_url, $options);
-	}
-
-
 	
-	function http_post ($url, $data)
-	{
-	    $data_url = http_build_query ($data);
-	    $data_len = strlen ($data_url);
 
-	    return array ('content'=>file_get_contents ($url, false, stream_context_create (array ('http'=>array ('method'=>'POST'
-	            , 'header'=>"Connection: close\r\nContent-Length: $data_len\r\n"
-	            , 'content'=>$data_url
-	            ))))
-	        , 'headers'=>$http_response_header
-	        );
+
+		$username=explode('@', $_POST['payer_email']);
+		$username=$username[0];
+
+
+
+
+
+		include 'wp-blog-header.php';
+		include 'wp-includes/registration.php';
+		include 'wp-includes/user.php';
+
+
+		$password=wp_generate_password();
+
+		// If the user doesn't exit, create it.
+		if(!username_exists($username) && !email_exists(($_POST['payer_email'])))
+		{
+			$user_id=wp_create_user($username, $password,$_POST['payer_email']);
+			if(is_int($user_id))
+			{
+				$user=new WP_User($user_id);
+				$user->set_role($role);
+			}
+		}
+		else
+		{
+			//If the user name already exists, only updated capabilities.
+
+		}
+
+
 	}
+
+
